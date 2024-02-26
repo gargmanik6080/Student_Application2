ca0a91b0f1f460b9cadd5e8870c12e16befe7c36
gargmanik6080 <gargmanik6080@gmail.com>
2024-02-26 21:10:40 +0530

    Fully Working

--- src/main/java/com/example/Student_Application2/resource/Student.java
+++ src/main/java/com/example/Student_Application2/resource/Student.java
@@ -1,3 +1,5 @@
+package com.example.Student_Application2.resource;
+
 import com.example.Student_Application2.model.StudentDataModel;
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,23 +8,39 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/student")
+
 public class Student {
 
     final public Map<String, StudentDataModel> idToStudent = new HashMap<>();
     final public Map<String, List<StudentDataModel>> universityToStudents = new HashMap<>();
 
+    @GetMapping
+    public List<StudentDataModel> getAllStudents(){
+        List<StudentDataModel> studentsList = new ArrayList<>();
+        for (StudentDataModel student : idToStudent.values()) {
+            studentsList.add(student);
+        }
+        if (studentsList.isEmpty()) {
+            throw new RuntimeException("No students found");
+        }
+        return studentsList;
+    }
+    
     @PostMapping("/addStudent")
-    public String addStudent(@RequestBody String name,
-                             @RequestBody String university,
-                             @RequestBody String aadhaar,
-                             @RequestBody Integer age){
+    // public String addStudent(@RequestBody String name,
+    //                          @RequestBody String university,
+    //                          @RequestBody String aadhaar,
+    //                          @RequestBody Integer age){
 
+    public String addStudent(@RequestBody com.example.Student_Application2.model.StudentDataModel student){
         String id = UUID.randomUUID().toString();
-        StudentDataModel student = new StudentDataModel(id, name, university, aadhaar, age);
+        // StudentDataModel student = new StudentDataModel(id, name, university, aadhaar, age);
+        student.id = id;
         idToStudent.put(id, student);
-        List<StudentDataModel> students = universityToStudents.getOrDefault(university, new ArrayList<>());
+
+        List<StudentDataModel> students = universityToStudents.getOrDefault(student.university, new ArrayList<>());
         students.add(student);
-        universityToStudents.put(university, students);
+        universityToStudents.put(student.university, students);
 
         return id;
     }
